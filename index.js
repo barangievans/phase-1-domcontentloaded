0dd7c99c9b13de3a233750b27481213d02aca709
barangievans <barangievans@gmail.com>
2024-07-14 21:02:36 +0300

    content

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// index.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paragraph = document.querySelector('p');
+    if (paragraph) {
+        paragraph.textContent = 'This is really cool!';
+    }
+});
+
